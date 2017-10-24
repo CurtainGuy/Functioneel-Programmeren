d6c40372fbe07835f9ae27b40f2cbf33869789a1
Wessel Gordijn <wesselsimon@hotmail.com>
2017-10-24 12:20:57 +0200

    Picture able to render

--- Final Project/src/View.hs
+++ Final Project/src/View.hs
@@ -3,6 +3,7 @@
 module View where
 
 import Graphics.Gloss
+import Graphics.Gloss.Game
 import Model
 
 view :: GameState -> IO Picture
@@ -13,3 +14,4 @@ viewPure gstate = case infoToShow gstate of
   ShowNothing   -> blank
   ShowANumber n -> color green (text (show n))
   ShowAChar   c -> color green (text [c])
+  ShowAPic    f -> png f
